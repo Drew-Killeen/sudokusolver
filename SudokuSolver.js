ae2e01a73f54844c2614d2e0a041d68ccf117dc6
Drew Killeen <drewkilleen00@gmail.com>
2022-04-13 09:08:49 -0400

    add solveBoard function

--- SudokuSolver.js
+++ SudokuSolver.js
@@ -1,52 +1,78 @@
 var board = [
-    [7, 0, 2, 0, 5, 0, 6, 0, 0],
-    [0, 0, 0, 0, 0, 3, 0, 0, 0],
-    [1, 0, 0, 0, 0, 9, 5, 0, 0],
-    [8, 0, 0, 0, 0, 0, 0, 9, 0],
-    [0, 4, 3, 0, 0, 0, 7, 5, 0],
-    [0, 9, 0, 0, 0, 0, 0, 0, 8],
-    [0, 0, 9, 7, 0, 0, 0, 0, 5],
-    [0, 0, 0, 2, 0, 0, 0, 0, 0],
-    [0, 0, 7, 0, 4, 0, 2, 0, 3]
+  [7, 0, 2, 0, 5, 0, 6, 0, 0],
+  [0, 0, 0, 0, 0, 3, 0, 0, 0],
+  [1, 0, 0, 0, 0, 9, 5, 0, 0],
+  [8, 0, 0, 0, 0, 0, 0, 9, 0],
+  [0, 4, 3, 0, 0, 0, 7, 5, 0],
+  [0, 9, 0, 0, 0, 0, 0, 0, 8],
+  [0, 0, 9, 7, 0, 0, 0, 0, 5],
+  [0, 0, 0, 2, 0, 0, 0, 0, 0],
+  [0, 0, 7, 0, 4, 0, 2, 0, 3],
 ];
 
 const GRID_SIZE = 9;
 
 function isNumInRow(num, row) {
-    for(let i = 0; i < GRID_SIZE; i++) {
-        if(board[row][i] == num) {
-            return true;
-        }
+  for (let i = 0; i < GRID_SIZE; i++) {
+    if (board[row][i] == num) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 function isNumInColumn(num, column) {
-    for(let i = 0; i < GRID_SIZE; i++) {
-        if(board[i][column] == num) {
-            return true;
-        }
+  for (let i = 0; i < GRID_SIZE; i++) {
+    if (board[i][column] == num) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 function isNumInSquare(num, row, column) {
-    row = Math.floor(row / 3) * 3;
-    column = Math.floor(column / 3) * 3;
+  row = Math.floor(row / 3) * 3;
+  column = Math.floor(column / 3) * 3;
 
-    for(let i = row; i < row + 3; i++) {
-        for(let j = column; j < column + 3; j++) {
-            if(board[i][j] == num) {
-                return true; 
-            }
-        }
+  for (let i = row; i < row + 3; i++) {
+    for (let j = column; j < column + 3; j++) {
+      if (board[i][j] == num) {
+        return true;
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 function isNumValid(num, row, column) {
-    if(isNumInColumn(num, column) || isNumInRow(num, row) || isNumInSquare(num, row, column)) {
+  if (
+    isNumInColumn(num, column) ||
+    isNumInRow(num, row) ||
+    isNumInSquare(num, row, column)
+  ) {
+    return false;
+  }
+  return true;
+}
+
+function solveBoard() {
+  for (let row = 0; row < GRID_SIZE; row++) {
+    for (let column = 0; column < GRID_SIZE; column++) {
+      if (board[row][column] == 0) {
+        for (let i = 1; i <= 9; i++) {
+          if (isNumValid(i, row, column)) {
+            board[row][column] = i;
+
+            if (solveBoard()) {
+              return true;
+            } else {
+              board[row][column] = 0;
+            }
+          }
+        }
         return false;
+      }
     }
-    return true;
+  }
+  return true;
 }
